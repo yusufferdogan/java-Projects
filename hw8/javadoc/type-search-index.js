9f56b00addbb52a63899468e1a99719ffa4757d6
yusufferdogan <45846424+yusufferdogan@users.noreply.github.com>
2021-02-15 17:56:09 +0300

    Delete hw8/javadoc directory

--- hw8/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"GTUContainer"},{"p":"<Unnamed>","l":"GTUIterator"},{"p":"<Unnamed>","l":"GtuSet"},{"p":"<Unnamed>","l":"GTUVector"},{"p":"<Unnamed>","l":"Main"}]
